b69970f587dc8b0c7a0cab1e81d7f9d197c1aa4b
Brady Bridges <bradyjbridges@gmail.com>
2019-09-11 21:20:51 -0600

    Add basic styles

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 $('nav').click((e) => {
   if(e.target.dataset.toggle) {
     const toggleTarget = e.target.dataset.toggle;
-    $(toggleTarget).siblings().addClass('display-none');
-    $(toggleTarget).toggleClass('display-none');
+    $(toggleTarget).siblings().fadeOut(500);
+    $(toggleTarget).fadeIn(500);
   } 
 })
