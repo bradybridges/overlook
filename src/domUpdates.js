7353ad998d407e83078ab95d3f5f01d3c05dc889
Brady Bridges <bradyjbridges@gmail.com>
2019-09-11 21:52:02 -0600

    Add mixins, updated tab behavior

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 const domUpdates = {
   updateHome(date, admin) {
     $('#home-date').text(date);
-    $('#percent-booked').text(`${admin.returnPercentBooked()}%`);
+    $('#percent-booked').text(`${admin.returnPercentBooked()}% Of Rooms Filled`);
     $('#num-rooms-avail').text(`${admin.returnNumRoomsAvailable()} Rooms Available`);
     $('#revenue').text(`$${admin.returnTodaysRevenue()}`);
   },
--- src/eventListeners.js
+++ src/eventListeners.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 $('nav').click((e) => {
   if(e.target.dataset.toggle) {
     const toggleTarget = e.target.dataset.toggle;
+    $(e.target).addClass('active-button');
+    $(e.target).siblings().removeClass('active-button');
     $(toggleTarget).siblings().fadeOut(500);
     $(toggleTarget).fadeIn(500);
   } 
